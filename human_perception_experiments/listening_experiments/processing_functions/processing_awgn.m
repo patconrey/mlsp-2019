9d36cb472ccd6b75970ff63d076f588142968dca
Mark Lindsey <markrlindsey@comcast.net>
2019-11-01 12:53:03 -0400

    Updated awgn function to fit standard parameters

--- human_perception_experiments/listening_experiments/processing_functions/processing_awgn.m
+++ human_perception_experiments/listening_experiments/processing_functions/processing_awgn.m
@@ -1,17 +1,13 @@
-function output_signal = processing_awgn(input_signal)
-    % This function is a black box to the rest of the
-    % experiment pipeline. It needs to take in a column vector waveform of arbitrary
-    % length and output a column vector waveform. The sampling frequency is assumed to be 16kHz.
-    % This function can do anything to the waveform; the only thing that matters is 
-    % its input and its output.
-    %
-    % Use this as a template for other processing blocks.
-    
+function [output_signal, snr] = processing_awgn(input_signal, snr)
     %% README
-    % This processing block adds white Gaussian noise to the signal at SNR -5dB.
+    
+    % This processing block adds white Gaussian noise to the signal at 
+    % random SNR (between -10 and 10dB).
     
     %% Script
-    
-    output_signal = awgn(input_signal, 10);
+    if nargin == 1
+        snr = generate_random_number([1 10]);
+    end
+    output_signal = awgn(input_signal, snr, 'measured');
 end
 
