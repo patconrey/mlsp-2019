3d962b910f4334d12d170248f016eaa0c94ba78b
Patrick Conrey <patrickconrey@gmail.com>
2019-11-03 15:18:18 -0500

    generating experiment sources

--- human_perception_experiments/generate_experiments.m
+++ human_perception_experiments/generate_experiments.m
@@ -8,6 +8,8 @@ addpath("listening_experiments/processing_functions");
 %     @processing_awgn, 4;
 % ];
 
+number_of_experiments = 10;
+
 max_duration_in_seconds = 2;
 
 path_to_processed_signals = "artifacts/";
@@ -44,4 +46,46 @@ path_to_audio_data = "../dataset/LA/ASVspoof2019_LA_eval/flac/";
 
 %% Load in protocol files
 
-protocol = readtable(path_to_protocol_file, "Delimiter", " ", "ReadVariableNames", false);
+protocol = table2array(readtable(path_to_protocol_file, "Delimiter", " ", "ReadVariableNames", false));
+
+indices_for_spoofed_data = protocol(:, 5) == "spoof";
+indices_for_bonafide_data = protocol(:, 5) == "bonafide";
+
+protocol_spoofed = protocol(indices_for_spoofed_data, :);
+protocol_bonafide = protocol(indices_for_bonafide_data, :);
+
+%% Create experiment sets
+
+% Experiment sources is an array of structs each with the following properties
+%   - label: Indicates which class has majority
+%   - spoofed_samples: A cell array of filenames corresponding to spoofed 
+%       audio samples. There will be 3 elements if label == "spoofed".
+%   - bonafide_samples: A cell array of filenames corresponding to bonafide 
+%       audio samples. There will be 3 elements if label == "bonafide".
+experiment_sources = [];
+for experiment_index = 1:number_of_experiments
+    is_majority_spoofed = randi([0 1], 1);
+    
+    experiment_label = "bonafide";
+    if is_majority_spoofed
+        experiment_label = "spoofed";
+    end
+    experiment_sources(experiment_index).label = experiment_label;
+    
+    number_spoofed_samples = 1;
+    if is_majority_spoofed
+        number_spoofed_samples = 3;
+    end
+    
+    number_bonafide_samples = 3;
+    if is_majority_spoofed
+        number_bonafide_samples = 1;
+    end
+    
+    % Draw samples
+    spoofed_samples = data_sample(protocol_spoofed, number_spoofed_samples);
+    bonafide_samples = data_sample(protocol_bonafide, number_bonafide_samples);
+    experiment_sources(experiment_index).spoofed_samples = spoofed_samples;
+    experiment_sources(experiment_index).bonafide_samples = bonafide_samples;
+end
+
