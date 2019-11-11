2987b9fd7eca42cc92b36db43fe0d930c61401ff
Patrick Conrey <patrickconrey@gmail.com>
2019-11-11 16:09:12 -0500

    got evaluation features

---
+++ ASVspoof_2019_baseline_CM_v1/generate_evaluation_features.m
@@ -0,0 +1,74 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ASVspoof 2019
+% Automatic Speaker Verification Spoofing and Countermeasures Challenge
+%
+% http://www.asvspoof.org/
+%
+% ============================================================================================
+% Matlab implementation of spoofing detection baseline system based on:
+%   - linear frequency cepstral coefficients (LFCC) features + Gaussian Mixture Models (GMMs)
+%   - constant Q cepstral coefficients (CQCC) features + Gaussian Mixture Models (GMMs)
+% ============================================================================================
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+clear; close all; clc;
+
+% add required libraries to the path
+addpath(genpath('LFCC'));
+addpath(genpath('CQCC_v1.0'));
+addpath(genpath('GMM'));
+addpath(genpath('bosaris_toolkit'));
+addpath(genpath('tDCF_v1'));
+
+% set here the experiment to run (access and feature type)
+access_type = 'LA'; % LA for logical or PA for physical
+feature_type = 'CQCC'; % LFCC or CQCC
+
+% set paths to the wave files and protocols
+
+% TODO: in this code we assume that the data follows the directory structure:
+%
+% ASVspoof_root/
+%   |- LA
+%      |- ASVspoof2019_LA_dev_asv_scores_v1.txt
+% 	   |- ASVspoof2019_LA_dev_v1/
+% 	   |- ASVspoof2019_LA_protocols_v1/
+% 	   |- ASVspoof2019_LA_train_v1/
+%   |- PA
+%      |- ASVspoof2019_PA_dev_asv_scores_v1.txt
+%      |- ASVspoof2019_PA_dev_v1/
+%      |- ASVspoof2019_PA_protocols_v1/
+%      |- ASVspoof2019_PA_train_v1/
+
+pathToASVspoof2019Data = '../dataset/';
+
+pathToDatabase = fullfile(pathToASVspoof2019Data, access_type);
+devProtocolFile = fullfile(pathToDatabase, horzcat('ASVspoof2019_', access_type, '_cm_protocols'), horzcat('ASVspoof2019.', access_type, '.cm.dev.trl.txt'));
+
+% read development protocol
+fileID = fopen(devProtocolFile);
+protocol = textscan(fileID, '%s%s%s%s%s');
+fclose(fileID);
+
+% get file and label lists
+filelist = protocol{2};
+attackType = protocol{4};
+key = protocol{5};
+
+% get indices of genuine and spoof files
+bonafideIdx = find(strcmp(key,'bonafide'));
+spoofIdx = find(strcmp(key,'spoof'));
+
+%% Feature extraction and scoring of development data
+
+% process each development trial: feature extraction and scoring
+evaluationFeatureCell = cell(size(filelist));
+parfor i=1:length(filelist)
+    filePath = fullfile(pathToDatabase,['ASVspoof2019_' access_type '_dev/flac'],[filelist{i} '.flac']);
+    [x,fs] = audioread(filePath);
+    evaluationFeatureCell{i} = cqcc(x, fs, 96, fs/2, fs/2^10, 16, 29, 'ZsdD');
+end
+
+save("evaluationFeatureCell_new.mat", "evaluationFeatureCell");
+
+disp('Done!');
--- baseline_model/train.py
+++ baseline_model/train.py
@@ -1,16 +1,16 @@
 import numpy as np
 import scipy.io as sio 
 from sklearn import mixture
-from utils import load_and_expand_data
+from utils import load_and_expand_training_data
 from joblib import load, dump
 
 # Config
-max_iterations = 20
+max_iterations = 10
 number_of_mixtures = 512
 covariance_type = "tied"
 
 # Load Data
-bonafide_features = load_and_expand_data("bonafideFeatureCell.mat")
+bonafide_features = load_and_expand_training_data("bonafideFeatureCell.mat")
 
 # Train GMM for bonafide class
 clf = mixture.GaussianMixture(n_components=number_of_mixtures, covariance_type=covariance_type, verbose=True, max_iter=max_iterations)
