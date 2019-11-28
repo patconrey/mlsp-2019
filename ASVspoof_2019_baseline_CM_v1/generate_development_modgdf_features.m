1baba4587e77c0f993987967ce18e627fa086b23
Patrick Conrey <patrickconrey@gmail.com>
2019-11-28 11:44:11 -0600

    updates and things

--- ASVspoof_2019_baseline_CM_v1/generate_development_modgdf_features.m
+++ ASVspoof_2019_baseline_CM_v1/generate_development_modgdf_features.m
@@ -19,14 +19,11 @@ addpath(genpath('CQCC_v1.0'));
 addpath(genpath('GMM'));
 addpath(genpath('bosaris_toolkit'));
 addpath(genpath('tDCF_v1'));
+addpath("../feature_extractors/modgdf");
 
 % set here the experiment to run (access and feature type)
 access_type = 'LA'; % LA for logical or PA for physical
-feature_type = 'CQCC'; % LFCC or CQCC
 
-% set paths to the wave files and protocols
-
-% TODO: in this code we assume that the data follows the directory structure:
 %
 % ASVspoof_root/
 %   |- LA
@@ -43,10 +40,10 @@ feature_type = 'CQCC'; % LFCC or CQCC
 pathToASVspoof2019Data = '../dataset/';
 
 pathToDatabase = fullfile(pathToASVspoof2019Data, access_type);
-devProtocolFile = fullfile(pathToDatabase, horzcat('ASVspoof2019_', access_type, '_cm_protocols'), horzcat('ASVspoof2019.', access_type, '.cm.dev.trl.txt'));
+protocolFile = fullfile(pathToDatabase, horzcat('ASVspoof2019_', access_type, '_cm_protocols'), horzcat('ASVspoof2019.', access_type, '.cm.dev.trl.txt'));
 
 % read development protocol
-fileID = fopen(devProtocolFile);
+fileID = fopen(protocolFile);
 protocol = textscan(fileID, '%s%s%s%s%s');
 fclose(fileID);
 
@@ -59,16 +56,30 @@ key = protocol{5};
 bonafideIdx = find(strcmp(key,'bonafide'));
 spoofIdx = find(strcmp(key,'spoof'));
 
-%% Feature extraction and scoring of development data
+%% Config
 
-% process each development trial: feature extraction and scoring
+DFT_LENGTH = 512;
+fs = 16000;
+
+%% Create Mel Filter Banks
+hz2mel = @(hz)(1127*log(1+hz/700)); % Hertz to mel warping function
+mel2hz = @(mel)(700*exp(mel/1127)-700); % mel to Hertz warping function
+
+number_of_filters = 32;
+length_of_each_filter = floor(DFT_LENGTH/2) + 1;
+frequency_limits = [0 fs/2];
+[ filter_bank, ~, ~ ] = trifbank( number_of_filters, length_of_each_filter, frequency_limits, fs, hz2mel, mel2hz );
+
+%% Feature extraction and scoring of data
+
 evaluationFeatureCell = cell(size(filelist));
 parfor i=1:length(filelist)
     filePath = fullfile(pathToDatabase,['ASVspoof2019_' access_type '_dev/flac'],[filelist{i} '.flac']);
     [x,fs] = audioread(filePath);
-    evaluationFeatureCell{i} = cqcc(x, fs, 96, fs/2, fs/2^10, 16, 29, 'ZsdD');
+    [~, cepstral_features, ~] = modified_group_delay_feature(x, fs);
+    evaluationFeatureCell{i} = cepstral_features;
 end
 
-save("evaluationFeatureCell_new.mat", "evaluationFeatureCell");
+save("dev_modgdf_features.mat", "evaluationFeatureCell");
 
 disp('Done!');
